7de2270f18e7f2185805aa0adb083f89a00192c3
Nathan Tefera <39712791+ntefera530@users.noreply.github.com>
2026-05-09 11:41:49 -0700

    Add: buy-and-sell-crypto - submission-3

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-3.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+        int max = 0;
+        
+        int start = 0;
+        int end = 1;
+
+        while(end < prices.length){
+            if(prices[start] < prices[end]){
+                max = Math.max(max, prices[end] - prices[start]);
+            }
+            else{
+                start = end;
+            }
+
+            end++;
+        }
+
+        //max = Math.max(max, prices[end - 1] - prices[start]);
+        return max;
+    }
+}
