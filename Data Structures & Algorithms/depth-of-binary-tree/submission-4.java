5d8428ed36962ef52cb0da57416f0fcd8078204a
Nathan Tefera <39712791+ntefera530@users.noreply.github.com>
2026-05-10 02:23:22 -0700

    Add: depth-of-binary-tree - submission-4

---
+++ Data Structures & Algorithms/depth-of-binary-tree/submission-4.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+class Solution {
+
+    int max = 0;    
+    public int maxDepth(TreeNode root) {
+        dfs(root, 1);
+        return max;
+    }
+
+    public void dfs(TreeNode root, int depth){
+        if(root == null){
+            return;
+        }
+
+        if(root.left == null && root.right == null){
+            max = Math.max(max, depth);
+        }
+
+        dfs(root.left, depth + 1);
+        dfs(root.right, depth + 1);
+    }
+}
