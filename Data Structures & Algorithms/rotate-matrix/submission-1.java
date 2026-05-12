62a304ef8236f80dacbabbfcfbfa74eadf9d1f8c
Nathan Tefera <39712791+ntefera530@users.noreply.github.com>
2026-05-11 19:58:51 -0700

    Add: rotate-matrix - submission-1

---
+++ Data Structures & Algorithms/rotate-matrix/submission-1.java
@@ -0,0 +1,25 @@
+class Solution {
+    public void rotate(int[][] matrix) {
+        int ROW = matrix.length;
+        int COL = matrix[0].length;
+
+        //verticle Slice
+        for(int i = 0; i < ROW / 2; i++){
+            for(int j = 0; j < COL; j++){
+                int buffer = matrix[i][j];
+                matrix[i][j] = matrix[ROW - 1- i][j];
+                matrix[ROW - 1- i][j] = buffer;
+            }
+        }
+
+        //Transpose
+        for(int i = 0; i < ROW; i++){
+            for(int j = i + 1; j < COL; j++){
+                int buffer = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = buffer;
+            }
+        }
+                                                                                                                                                                                                         
+    }
+}
