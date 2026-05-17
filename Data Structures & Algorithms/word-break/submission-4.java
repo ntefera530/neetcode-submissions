2cfb168e8c787e9d5b75eb46eb8fc76fc659902e
Nathan Tefera <39712791+ntefera530@users.noreply.github.com>
2026-05-17 13:37:38 -0700

    Add: word-break - submission-4

---
+++ Data Structures & Algorithms/word-break/submission-4.java
@@ -0,0 +1,44 @@
+class Solution {
+    HashMap<String, Integer> letters;
+    public boolean wordBreak(String s, List<String> wordDict) {
+        int[] memo = new int[s.length() + 1];
+        letters = new HashMap<>();
+        for(String word : wordDict){
+            letters.put(word, word.length());
+        }
+
+        return dfs(s, wordDict,0, memo);
+    }
+
+    public boolean dfs(String s, List<String> wordDict, int index, int[] memo){
+        if(memo[index] == 1){
+            return false;
+        }
+        //0 = uncomputed, 1 = false, 2 = true;
+        if(memo[index] == 2){
+            return true;
+        }
+        if(index == s.length()){
+            return true;
+        }   
+
+        if(index > s.length()){
+            return false;
+        }
+
+
+        boolean found = false;
+        for(String word : wordDict){
+            int length = word.length();
+            
+            //String found
+            if(index + length <= s.length() && s.substring(index, index + length).equals(word)){
+                found |= dfs(s, wordDict, index + length, memo);
+            }
+        }
+
+        memo[index] = (found == true) ? 2 : 1;
+
+        return (memo[index] == 2) ? true : false;
+    }
+}
