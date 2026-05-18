ecf5ded97aa7a0734e5c800883bd9073d0def779
Nathan Tefera <39712791+ntefera530@users.noreply.github.com>
2026-05-17 17:25:34 -0700

    Add: longest-common-subsequence - submission-0

---
+++ Data Structures & Algorithms/longest-common-subsequence/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int longestCommonSubsequence(String text1, String text2) {
+        int length1 = text1.length();
+        int length2 = text2.length();
+
+        int[][] dp = new int[length1 + 1][length2 + 1];
+
+        for(int i = length1 - 1; i >= 0; i--){
+            for(int j = length2 - 1; j >= 0; j--){
+                if (text1.charAt(i) == text2.charAt(j)) {
+                    dp[i][j] = 1 + dp[i + 1][j + 1];
+                } else {
+                    dp[i][j] = Math.max(dp[i][j + 1], dp[i + 1][j]);
+                } 
+            }
+        }
+
+        return dp[0][0];
+
+    }
+}
