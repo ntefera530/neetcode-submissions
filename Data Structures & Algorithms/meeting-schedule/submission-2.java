d35e8f5b3465c18e23ffa147ffe35bbb9b8ce05d
Nathan Tefera <39712791+ntefera530@users.noreply.github.com>
2026-07-26 17:18:42 -0700

    Add: meeting-schedule - submission-2

---
+++ Data Structures & Algorithms/meeting-schedule/submission-2.java
@@ -0,0 +1,26 @@
+/**
+ * Definition of Interval:
+ * public class Interval {
+ *     public int start, end;
+ *     public Interval(int start, int end) {
+ *         this.start = start;
+ *         this.end = end;
+ *     }
+ * }
+ */
+
+class Solution {
+    public boolean canAttendMeetings(List<Interval> intervals) {
+        Collections.sort(intervals, Comparator.comparingInt(i -> i.start));
+
+        for(int i = 1; i < intervals.size(); i++){
+            Interval one = intervals.get(i - 1);
+            Interval two = intervals.get(i);
+            if(one.end > two.start){
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
